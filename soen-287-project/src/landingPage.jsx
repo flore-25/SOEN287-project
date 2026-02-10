8376242a445bd937733f9a08ae0e305302378772
AaronDesrosiers <desrosiers.aaron@gmail.com>
2026-02-10 15:31:56 -0500

    Added CSS Styling to landing page

--- soen-287-project/src/landingPage.jsx
+++ soen-287-project/src/landingPage.jsx
@@ -5,16 +5,16 @@ function LandingPage()
 {
   const [loggedIn, setLogIn] = useState(false)
     return(
-    <>
+    <div className='landingPage'>
         <div>
             <TopNavBar />
         </div>
         <div className='picture'>
             <a target='_blank'>
-                <img src={landingBG} className='logo landingBG' alt='Landing Background' />
+                <img src={landingBG} className='logoLandingBG' alt='Landing Background' />
             </a>
         </div>
-    </>
+    </div>
     )
   
 }
@@ -27,12 +27,12 @@ function TopNavBar()
         <nav className='navbar'>
             <div className='navbar-left'>
                 <a href='/' className='logo'>
-                    SOEN Project
+                    SOEN Project cool
                 </a>
             </div>
 
             <div className='navbar-right'>
-                <a href='/login' className='login_button'>
+                <a href='/login' className='login-button'>
                     Sign Up
                 </a>
             </div>
--- soen-287-project/src/main.jsx
+++ soen-287-project/src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './landingPage.css'
 import LandingPage from './landingPage.jsx'
 
 createRoot(document.getElementById('root')).render(

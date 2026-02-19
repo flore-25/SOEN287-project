4376ca7e3b63a555288487a9d7e33ad3e6c9eed1
AaronDesrosiers <desrosiers.aaron@gmail.com>
2026-02-19 14:34:58 -0500

    started vertical nav pane

--- soen-287-project/src/main.jsx
+++ soen-287-project/src/main.jsx
@@ -9,13 +9,14 @@ import {
 import './index.css'
 import LandingPage from './landingPage.jsx'
 import LoginPage from './loginPage.jsx'
+import Navbar from './vertNavPane.jsx';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Router>
       <Routes>
         <Route path="/" Component={LandingPage}/>
-        <Route path="/login" Component={LoginPage}/>
+        <Route path="/login" Component={Navbar}/>
       </Routes>
     </Router>
   </StrictMode>
